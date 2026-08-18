251dbed8ac83ee5547d3b3133d71cfb1bebe39ec
Nathanial <Nathanialcodes@gmail.com>
2026-08-18 18:37:07 +0100

    Streamlit app deploy

---
+++ app/streamlit_app.py
@@ -0,0 +1,6 @@
+
+import streamlit as st
+
+st.set_page_config(page_title="UK Capital Allowances", layout="wide")
+st.title("UK Capital Allowances and Manufacturing Investment")
+st.write("Deployment test.")
